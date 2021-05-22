05cb277a7f58f8216e5fa3cb886f079b70f11d64
philjacks <77470834+philjacks@users.noreply.github.com>
2021-05-22 22:35:58 +0100

    complete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-document.querySelectorAll(".q-btn").forEach((item) => {
+const questions = document.querySelectorAll(".q-btn");
+
+questions.forEach((item) => {
   item.addEventListener("click", (e) => {
+    item.firstElementChild.classList.toggle("flipped");
     item.classList.toggle("bold");
     item.nextElementSibling.classList.toggle("active");
   });
